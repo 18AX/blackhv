0edd9cbc25dd15745adcecd3ff73540a0e8eb442
Alex Lev <levzx18@gmail.com>
2023-01-11 14:35:23 +0100

    tests: add more tests on the queue object

--- src/tests.c
+++ src/tests.c
@@ -11,3 +11,44 @@ Test(queue, queue_create)
     cr_assert_eq(q->front, 0);
     cr_assert_eq(q->rear, 0);
 }
+
+Test(queue, queue_add_pop)
+{
+    queue_t *q = queue_new(512);
+
+    char value = 'A';
+
+    cr_assert_eq(queue_write(q, (unsigned char *)&value, sizeof(char)), 1);
+
+    char readed = 'A';
+
+    cr_assert_eq(queue_read(q, (unsigned char *)&readed, sizeof(char)), 1);
+
+    cr_assert_eq(value, readed);
+    cr_assert_eq(queue_empty(q), 1);
+    cr_assert_eq(queue_read(q, (unsigned char *)&readed, sizeof(char)), 0);
+}
+
+Test(queue, queue_add_multiple)
+{
+    queue_t *q = queue_new(512);
+
+    for (unsigned char i = 0; i < 16; ++i)
+    {
+        cr_assert_eq(queue_write(q, &i, sizeof(unsigned char)), 1);
+    }
+
+    for (unsigned char i = 0; i < 16; ++i)
+    {
+        unsigned char readed = 'B';
+
+        cr_assert_eq(queue_read(q, &readed, sizeof(unsigned char)), 1);
+
+        cr_assert_eq(i, readed);
+    }
+
+    unsigned char r = 0;
+
+    cr_assert_eq(queue_empty(q), 1);
+    cr_assert_eq(queue_read(q, &r, sizeof(char)), 0);
+}
